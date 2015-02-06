0a4b833993e37eb277e12e9a8a3d223afe1d5972
Kevin Nguyen <kvn011@ucsd.edu>
2015-02-05 21:45:32 -0800

    i hate MY LIFE

--- web.js
+++ web.js
@@ -1,36 +1,5 @@
 var express = require('express');
 var app = express();
  
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var handlebars = require('express3-handlebars')
-
-var index = require('./index');
-
-// Example route
-// Example route
-// var user = require('./routes/user');
-
-// all environments
-var express = require('express'),
-var app = express();
-app.use(express.static('www'));
-app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
-});
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-// Add routes here
-app.get('/', index.view);
-// Example route
-// app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+app.use(express.static(__dirname + "/"));
+app.listen(process.env.PORT || 5000);
