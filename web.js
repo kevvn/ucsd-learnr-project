5e47f44ef47d40d448ed901fb90d90852ccab31e
Kevin Nguyen <kvn011@ucsd.edu>
2015-02-05 21:36:57 -0800

    PRETTY PLS 2.0

--- web.js
+++ web.js
@@ -1,5 +1,44 @@
 var express = require('express');
 var app = express();
  
-app.use(express.static(__dirname + "/www"));
-app.listen(process.env.PORT || 5000);
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var handlebars = require('express3-handlebars')
+
+var index = require('./index');
+
+// Example route
+// Example route
+// var user = require('./routes/user');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('Intro HCI secret key'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// Add routes here
+app.get('/', index.view);
+// Example route
+// app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
