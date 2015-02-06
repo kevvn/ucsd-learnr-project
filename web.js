9400ae59644339a09d1a7ea9e6f39017c49c8e20
Kevin Nguyen <kvn011@ucsd.edu>
2015-02-05 21:43:12 -0800

    i hate this

--- web.js
+++ web.js
@@ -12,22 +12,14 @@ var index = require('./index');
 // Example route
 // var user = require('./routes/user');
 
-var app = express();
-
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('Intro HCI secret key'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+var express = require('express'),
+var app = express();
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
 
 // development only
 if ('development' == app.get('env')) {
