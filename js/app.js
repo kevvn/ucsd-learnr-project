2f6a81c1b870c6a142e4f889891349abc4ba09a6
Kevin Nguyen <kvn011@ucsd.edu>
2015-02-26 21:03:23 -0800

    make it better

---
+++ js/app.js
@@ -0,0 +1,125 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+      Parse.initialize("qCKMCAPQBXhmMghKpKjkkNGhejWQ5w7Sm2NpYmnH", "NCteUlF8BFnxNniGAi6JqxbxxL5hKcUXlR4uECnn");
+      //Parse.initialize("8lI28e5lGH37hrHAcbLDVuLM9xXPWTrHCPG4rY8L", "JyA0uBxQrL4SLqZ6tdIT4CKY1og7PW7ryCquxy8m");
+
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
+  })
+
+  .state('app.help', {
+    url: "/help",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/help.html"
+      }
+    }
+  })
+
+  .state('app.login', {
+    url: "/login",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/login.html"
+      }
+    }
+  })
+
+  .state('app.signup', {
+    url: "/signup",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/signup.html"
+      }
+    }
+  })
+
+  .state('app.search', {
+    url: "/search",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/search.html"
+      }
+    }
+  })
+  .state('app.post', {
+    url: "/post",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/post.html",
+		controller: 'PostCtrl'
+      }
+    }
+  })
+
+  .state('app.404', {
+    url: "/404",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/404.html"
+      }
+    }
+  })
+
+    .state('app.playlists', {
+      url: "/playlists",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/playlists.html",
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.playlist', {
+    url: "/playlists/:playlistId",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/playlist.html" //,
+      //  controller: 'PlaylistCtrl'
+      }
+    }
+
+    })
+
+  .state('app.favorites', {
+    url: "/favorites/:userId",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/favorites.html",
+        controller: 'FavoriteCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
+});
